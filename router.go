6a304a17b7c669d3c4f625e35d9a8179c132d6be
ewangplay <ewangplay@gmail.com>
2017-05-16 10:28:30 +0800

    support multiple resources

--- router.go
+++ router.go
@@ -25,26 +25,48 @@ func (this *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	header.Add("Content-Type", "application/json")
 	header.Add("charset", "UTF-8")
 
-	resource, version, err := this.ParseURL(r.RequestURI)
+	version, resources, err := this.ParseURL(r.RequestURI)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		io.WriteString(w, MakeErrorResult(-1, err.Error()))
-	} else {
-		this_handler, err := this.GetHandler(resource)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			io.WriteString(w, MakeErrorResult(-1, err.Error()))
-		} else {
-			result, err := this_handler.Process(r, version, resource, this_handler.ProcessFunc)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				io.WriteString(w, result) //MakeErrorResult(-1, err.Error()))
-			} else {
-				header.Add("Content-Length", fmt.Sprintf("%v", len(result)))
-				io.WriteString(w, result)
-			}
-		}
+        return
 	}
+
+    var res []string
+    var resource string
+    for i, r := range resources {
+        if i == 0 {
+            resource = r
+        } else {
+            resource += fmt.Sprintf("/%s", r)
+        }
+        res = append(res, resource)
+    }
+
+    var this_handler Handler
+    var result string
+
+    for i := len(res)-1; i >= 0; i-- {
+        resource := res[i]
+
+        this_handler, err = this.GetHandler(resource)
+        if err == nil {
+            result, err = this_handler.Process(r, version, resource, this_handler.ProcessFunc)
+            if err != nil {
+                w.WriteHeader(http.StatusInternalServerError)
+                io.WriteString(w, result) //MakeErrorResult(-1, err.Error()))
+            } else {
+                header.Add("Content-Length", fmt.Sprintf("%v", len(result)))
+                io.WriteString(w, result)
+            }
+            return
+        }
+    }
+
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        io.WriteString(w, MakeErrorResult(-1, err.Error()))
+    }
 
 	return
 }
@@ -61,8 +83,7 @@ func (this *Router) GetHandler(resource string) (Handler, error) {
 //
 //通过正则表达式选择路由程序
 //
-func (this *Router) ParseURL(url string) (resource string, version int, err error) {
-	//确定是否是本服务能提供的控制类型
+func (this *Router) ParseURL(url string) (version int, resources []string, err error) {
 	urlPattern := "/v(\\d+)/(\\w+)"
 	urlRegexp, err := regexp.Compile(urlPattern)
 	if err != nil {
@@ -82,9 +103,10 @@ func (this *Router) ParseURL(url string) (resource string, version int, err erro
 
 	versionNum, _ := strconv.ParseInt(matchs[1], 10, 8)
 	version = int(versionNum)
-	resource = matchs[2]
 
-    //TODO: 把URL中的多级资源保存
+    for i := 2; i < len(matchs); i++ {
+        resources = append(resources, matchs[i]
+    }
 
 	return
 }
